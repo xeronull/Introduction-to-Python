ef053e85bc4167bc8536120e7ec4a00549b18b83
@xeronull <37910411+xeronull@users.noreply.github.com>
2022-03-10 11:57:27 -0500

    Add files via upload

---
+++ The print() function.py
@@ -0,0 +1,13 @@
+#The print() function
+
+print("Hello world!")
+print("This is how we print to the screen.")
+
+#Use print() without any arguments to print a blank line
+print()
+print("What just happened?")
+
+#By default, the print function ends with a newline. Passing the whitespace to the end parameter (end=' ') 
+#indicates that the end character has to be identified by whitespace and not a newline.
+print("This is a message on one line.", end=" ")
+print("This is a message on the same line.")
