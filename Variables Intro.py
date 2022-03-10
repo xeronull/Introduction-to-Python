958341e918cd6ac564007e307eeb1b69dd28b437
@xeronull <37910411+xeronull@users.noreply.github.com>
2022-03-10 12:22:15 -0500

    Create Variables Intro.py

---
+++ Variables Intro.py
@@ -0,0 +1,20 @@
+#Variables Intro
+
+#x is a container that holds a value of 5
+x = 5
+
+print(5)
+print(x)
+
+#Assigning informative variable names
+#The variable here is message and the variable value is Hello world.
+message = "Hello world!"
+print(message)
+
+#Always use Python built in names and functions as variables
+potato = "Goodbye World!"
+print(potato)
+
+#Bad idea to override built in names
+print = "Goodbye world!"
+print(print)
